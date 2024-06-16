73abae9b7ff86aef832b9e59f23eb4495ba23fb0
usfree <tlrmsjtm77@naver.com>
2024-06-17 04:52:16 +0900

    에셋 형상관리 - 1

---
+++ Assets/PublicAssets/UI/Scripts/Other/ExitToSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Michsky.UI.Shift
+{
+    public class ExitToSystem : MonoBehaviour
+    {
+        public void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+                        Application.Quit();
+#endif
+        }
+    }
+}
