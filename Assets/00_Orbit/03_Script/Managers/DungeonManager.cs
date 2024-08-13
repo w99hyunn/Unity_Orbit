a848e02c6832457e62ff1db0f4b3ef59060b2f07
starting-run <tlrmsjtm77@naver.com>
2024-08-14 01:41:30 +0900

    아레테 격파 성공시 이미지 변경

--- Assets/00_Orbit/03_Script/Managers/DungeonManager.cs
+++ Assets/00_Orbit/03_Script/Managers/DungeonManager.cs
@@ -11,6 +11,7 @@ public class DungeonManager : MonoBehaviour
     public TMP_Text text2;
     public TMP_Text text3;
     public Image backgroundImageBase;
+    public Sprite defaultBackground;
     public Sprite successBackground;
     public Sprite failureBackground;
 
@@ -33,17 +34,21 @@ public class DungeonManager : MonoBehaviour
      * 던전 로딩 화면
      * UnityEvent로 애니메이션과 코루틴 처리
      */
-    public void UpdateDungeonLoading(string t1, string t2, string t3, bool success = true)
+    public void UpdateDungeonLoading(string t1, string t2, string t3, int success = 0)
     {
         onDungeonEnter.Invoke();
 
-        if (true == success)
+        switch (success)
         {
-            backgroundImageBase.sprite = successBackground;
-        }
-        else if (false == success)
-        {
-            backgroundImageBase.sprite = failureBackground;
+            case 0:
+                backgroundImageBase.sprite = defaultBackground;
+                break;
+            case 1:
+                backgroundImageBase.sprite = successBackground;
+                break;
+            case 2:
+                backgroundImageBase.sprite = failureBackground;
+                break;
         }
 
         text1.text = t1;
--- Assets/00_Orbit/03_Script/Map/DungeonTimer.cs
+++ Assets/00_Orbit/03_Script/Map/DungeonTimer.cs
@@ -68,7 +68,7 @@ public class DungeonTimer : MonoBehaviour
 
     IEnumerator HandleFailure()
     {
-        DungeonManager.Instance.UpdateDungeonLoading("시간이 초과되었습니다.", "아레테를 파괴하지 못했습니다!", "잠시 후 원래 있던 곳으로 돌아갑니다.", false);
+        DungeonManager.Instance.UpdateDungeonLoading("시간이 초과되었습니다.", "아레테를 파괴하지 못했습니다!", "잠시 후 원래 있던 곳으로 돌아갑니다.", 2);
         yield return new WaitForSeconds(0f);
 
         StartCoroutine(LoadOutdoorsSceneAfterDelay(3f));
