ac1b4fc064f7d4def0ae79ed0e95cdb98b32417b
Woohyun Cho <tlrmsjtm77@naver.com>
2024-08-21 23:59:20 +0900

    아레테 파괴시 OnDeath 추가 및 네비메시 리빌드

--- Assets/00_Orbit/03_Script/Enemy/Arete.cs
+++ Assets/00_Orbit/03_Script/Enemy/Arete.cs
@@ -5,10 +5,11 @@ public class Arete : MonoBehaviour
     public DungeonTimer dungeonTimer;
     public EnemyMemoryPool enemySpawnTrigger;
 
-    private void OnDestroy()
+    public void OnDestroy()
     {
         Destroy(enemySpawnTrigger);
         UIManager.Instance.ScriptText_Enable($"{GameManager.Instance.currentZoneName}이 해방됐다. 더 이상 드론이 소환되지 않을거야.");
         dungeonTimer.Destroy_Arete();
+        Destroy(this.gameObject);
     }
 }
