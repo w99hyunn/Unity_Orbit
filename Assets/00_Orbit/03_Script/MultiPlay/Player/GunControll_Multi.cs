e24596b919a69b9a62f8db5a3a5fb6aff5b8cc33
Woohyun Cho <tlrmsjtm77@naver.com>
2024-10-01 04:24:18 +0900

    hitAim 표시 시간 변경

--- Assets/00_Orbit/03_Script/MultiPlay/Player/GunControll_Multi.cs
+++ Assets/00_Orbit/03_Script/MultiPlay/Player/GunControll_Multi.cs
@@ -133,7 +133,7 @@ namespace STARTING
             }
             hitAimImage.alpha = 1f;
 
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(0.1f);
 
             elapsedTime = 0f;
 
--- Assets/00_Orbit/03_Script/SinglePlay/Player/GunControll.cs
+++ Assets/00_Orbit/03_Script/SinglePlay/Player/GunControll.cs
@@ -130,7 +130,7 @@ namespace STARTING
             }
             hitAimImage.alpha = 1f;
 
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(0.1f);
 
             elapsedTime = 0f;
 
