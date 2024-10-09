61332b500f4cf83c255e1dea94c4eb948b6b8d7d
Woohyun Cho <tlrmsjtm77@naver.com>
2024-10-10 00:50:00 +0900

    정리

--- Assets/00_Orbit/03_Script/SinglePlay/Npc/WeaponChangeNpc.cs
+++ Assets/00_Orbit/03_Script/SinglePlay/Npc/WeaponChangeNpc.cs
@@ -1,74 +1,35 @@
-using STARTING;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-public class WeaponChangeNpc : MonoBehaviour
+namespace STARTING
 {
-    public List<GameObject> itemList;
-    public DissolveChilds dissolveChilds;
-
-    //public delegate void DestroyedEventHandler();
-    //public event DestroyedEventHandler OnDestroyed;
-
-    [Header("루트 사운드")]
-    public AudioClip rootSound;
-    public AudioSource audioSource;
-
-    private bool _isPlayerInTrigger = false;
-
-    private void Update()
+    public class WeaponChangeNpc : MonoBehaviour
     {
-        if (_isPlayerInTrigger && Input.GetKeyDown(KeyCode.F))
+        private bool _isPlayerInTrigger = false;
+
+        private void Update()
         {
-            UIManager.Instance.OpenChangeWeapon();
-            //StartCoroutine(dissolveChilds.AnimateDissolve());
-            //PlaySound(rootSound);
-            //StartCoroutine(DestroyAfterDelay(1f));
+            if (_isPlayerInTrigger && Input.GetKeyDown(KeyCode.F))
+            {
+                UIManager.Instance.OpenChangeWeapon();
+            }
         }
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        private void OnTriggerEnter(Collider other)
         {
-            _isPlayerInTrigger = true;
-            UIManager.Instance.interactionKeyEnable("무기 상점", "F");
+            if (other.CompareTag("Player"))
+            {
+                _isPlayerInTrigger = true;
+                UIManager.Instance.interactionKeyEnable("보급소", "F");
+            }
         }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        private void OnTriggerExit(Collider other)
         {
-            _isPlayerInTrigger = false;
-            UIManager.Instance.interactionKeyDisable();
-        }
-    }
-
-    //private IEnumerator DestroyAfterDelay(float delay)
-    //{
-    //    yield return new WaitForSeconds(delay);
-
-    //    //루트박스에서 F를 누르면 랜덤 아이템 생성
-    //    int randomIndex = Random.Range(0, itemList.Count);
-    //    GameObject selectedItem = itemList[randomIndex];
-
-    //    // 현재 위치에 y값 +1해서 스폰
-    //    Vector3 currentPosition = this.gameObject.transform.position;
-    //    Vector3 newPosition = new Vector3(currentPosition.x, currentPosition.y + 1, currentPosition.z);
-    //    Instantiate(selectedItem, newPosition, this.gameObject.transform.rotation);
-
-    //    OnDestroyed?.Invoke();
-    //    Destroy(gameObject);
-    //}
-
-    public void PlaySound(AudioClip clip)
-    {
-        if (audioSource != null && clip != null)
-        {
-            audioSource.PlayOneShot(clip);
+            if (other.CompareTag("Player"))
+            {
+                _isPlayerInTrigger = false;
+                UIManager.Instance.interactionKeyDisable();
+            }
         }
     }
 }
